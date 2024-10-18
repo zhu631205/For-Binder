9ab7da7f6228f5332e53299afd837b0745b818e8
zhu631205 <159397789+zhu631205@users.noreply.github.com>
2024-10-18 16:36:18 -0500

    Delete install.R

--- install.R
+++
@@ -1,5 +0,0 @@
-
-if (!requireNamespace("devtools", quietly = TRUE)) install.packages("devtools")
-if (!requireNamespace("BiocManager", quietly = TRUE)) install.packages("BiocManager")
-devtools::install_github("GreenleafLab/ArchR", ref="master", repos = BiocManager::repositories())
-ArchR::installExtraPackages()
