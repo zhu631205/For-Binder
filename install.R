d0dda687e3a4dc351bce595639c0a3f297ce472f
zhu631205 <159397789+zhu631205@users.noreply.github.com>
2024-10-19 09:12:30 -0500

    Update install.R

--- install.R
+++ install.R
@@ -1,7 +1,15 @@
+# install.R
+
 # Install devtools if it's not installed (to install Seurat from GitHub)
 if (!requireNamespace("devtools", quietly = TRUE)) {
   install.packages("devtools")
 }
 
-# Install Seurat version 5 from GitHub using devtools
-devtools::install_github("satijalab/seurat", ref = "v5.0.0")
+# Try installing Seurat v5 from GitHub
+tryCatch({
+  devtools::install_github("satijalab/seurat", ref = "v5.0.0")
+}, error = function(e) {
+  message("Error installing Seurat: ", e$message)
+  quit(status = 1)
+})
+
